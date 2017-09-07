86186dd8e890f790c77d30a9065465a4b2647192
japhak <yafoak@gmail.com>
2017-09-07 11:41:14 -0400

    New push

---
+++ src/test2/ForGit.java
@@ -0,0 +1,12 @@
+package test2;
+
+public class ForGit {
+
+	public static void main(String[] args) {
+		String firstName;
+		firstName= "JAPHETH";
+		System.out.println("My name is " + firstName);
+
+	}
+
+}
